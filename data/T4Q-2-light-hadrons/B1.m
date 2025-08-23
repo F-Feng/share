a206f3cac67b15e684e160b3f2799a8be1737356
Feng Feng <F.Feng@outlook.com>
2025-08-23 21:21:15 +0800

    Add files via upload

---
+++ data/T4Q-2-light-hadrons/B1.m
@@ -0,0 +1,55 @@
+(* Created with the Wolfram Language : www.wolfram.com *)
+((-5*Subscript[n, 4])/(2*Subscript[d, 2]*Subscript[d, 3]*Subscript[d, 5]*
+    Subscript[d, 6]*Subscript[d, 10]*Subscript[d, 11]*Subscript[d, 14]*
+    Subscript[d, 15]*Subscript[d, 16]) + (80*Subscript[n, 4])/
+   (9*Subscript[d, 2]*Subscript[d, 3]*Subscript[d, 9]*Subscript[d, 10]*
+    Subscript[d, 11]*Subscript[d, 14]*Subscript[d, 19]) - 
+  (80*Subscript[n, 7])/(9*Subscript[d, 4]*Subscript[d, 5]*Subscript[d, 7]*
+    Subscript[d, 12]*Subscript[d, 15]*Subscript[d, 17]*Subscript[d, 20]) - 
+  (5*Subscript[n, 7])/(2*Subscript[d, 2]*Subscript[d, 5]*Subscript[d, 6]*
+    Subscript[d, 7]*Subscript[d, 10]*Subscript[d, 15]*Subscript[d, 16]*
+    Subscript[d, 17]*Subscript[d, 20]) + (5*Subscript[n, 2]*Subscript[n, 7])/
+   (Subscript[d, 2]*Subscript[d, 5]*Subscript[d, 7]*Subscript[d, 9]*
+    Subscript[d, 10]*Subscript[d, 15]*Subscript[d, 17]*Subscript[d, 18]*
+    Subscript[d, 20]) + (5*Subscript[n, 7]*Subscript[n, 8])/
+   (Subscript[d, 3]*Subscript[d, 5]*Subscript[d, 6]*Subscript[d, 7]*
+    Subscript[d, 12]*Subscript[d, 15]*Subscript[d, 16]*Subscript[d, 17]*
+    Subscript[d, 20]) + (40*Subscript[n, 9])/(9*Subscript[d, 2]*
+    Subscript[d, 5]*Subscript[d, 6]*Subscript[d, 10]*Subscript[d, 14]*
+    Subscript[d, 15]*Subscript[d, 16]*Subscript[d, 22]) - 
+  (40*Subscript[n, 7]*Subscript[n, 10])/(9*Subscript[d, 2]*Subscript[d, 5]*
+    Subscript[d, 6]*Subscript[d, 7]*Subscript[d, 10]*Subscript[d, 14]*
+    Subscript[d, 15]*Subscript[d, 16]*Subscript[d, 17]*Subscript[d, 20]*
+    Subscript[d, 21]) - (5*Subscript[n, 7]*Subscript[n, 11])/
+   (2*Subscript[d, 2]*Subscript[d, 3]*Subscript[d, 5]*Subscript[d, 6]*
+    Subscript[d, 7]*Subscript[d, 10]*Subscript[d, 11]*Subscript[d, 14]*
+    Subscript[d, 15]*Subscript[d, 16]*Subscript[d, 17]*Subscript[d, 20]) - 
+  (5*Subscript[n, 7]*Subscript[n, 13])/(2*Subscript[d, 2]*Subscript[d, 3]*
+    Subscript[d, 5]*Subscript[d, 6]*Subscript[d, 7]*Subscript[d, 10]*
+    Subscript[d, 11]*Subscript[d, 15]*Subscript[d, 16]*Subscript[d, 17]*
+    Subscript[d, 20]) - (5*Subscript[n, 4]*Subscript[n, 14])/
+   (2*Subscript[d, 2]*Subscript[d, 3]*Subscript[d, 5]*Subscript[d, 6]*
+    Subscript[d, 8]*Subscript[d, 10]*Subscript[d, 11]*Subscript[d, 14]*
+    Subscript[d, 15]*Subscript[d, 16]*Subscript[d, 18]) - 
+  (5*Subscript[n, 15])/(18*Subscript[d, 2]*Subscript[d, 3]*Subscript[d, 5]*
+    Subscript[d, 6]*Subscript[d, 10]*Subscript[d, 11]*Subscript[d, 13]*
+    Subscript[d, 14]*Subscript[d, 15]*Subscript[d, 16]*Subscript[d, 21]) - 
+  (5*Subscript[n, 16])/(2*Subscript[d, 2]*Subscript[d, 3]*Subscript[d, 5]*
+    Subscript[d, 6]*Subscript[d, 10]*Subscript[d, 11]*Subscript[d, 13]*
+    Subscript[d, 15]*Subscript[d, 16]*Subscript[d, 20]) - 
+  (5*Subscript[n, 17])/(2*Subscript[d, 2]*Subscript[d, 3]*Subscript[d, 5]*
+    Subscript[d, 6]*Subscript[d, 10]*Subscript[d, 11]*Subscript[d, 13]*
+    Subscript[d, 15]*Subscript[d, 16]*Subscript[d, 21]) - 
+  (5*Subscript[n, 18])/(9*Subscript[d, 2]*Subscript[d, 5]*Subscript[d, 6]*
+    Subscript[d, 10]*Subscript[d, 13]*Subscript[d, 14]*Subscript[d, 15]*
+    Subscript[d, 16]*Subscript[d, 20]) - (5*Subscript[n, 18])/
+   (2*Subscript[d, 2]*Subscript[d, 3]*Subscript[d, 5]*Subscript[d, 6]*
+    Subscript[d, 10]*Subscript[d, 11]*Subscript[d, 13]*Subscript[d, 14]*
+    Subscript[d, 15]*Subscript[d, 16]*Subscript[d, 20]) + 
+  (10*Subscript[n, 19])/(Subscript[d, 2]*Subscript[d, 3]*Subscript[d, 6]*
+    Subscript[d, 9]*Subscript[d, 10]*Subscript[d, 12]*Subscript[d, 14]*
+    Subscript[d, 16]*Subscript[d, 18]*Subscript[d, 21]) + 
+  (5*Subscript[n, 20])/(2*Subscript[d, 2]*Subscript[d, 3]*Subscript[d, 5]*
+    Subscript[d, 6]*Subscript[d, 8]*Subscript[d, 10]*Subscript[d, 11]*
+    Subscript[d, 14]*Subscript[d, 15]*Subscript[d, 16]*Subscript[d, 18]*
+    Subscript[d, 21]))/(512*Sqrt[6])
---
+++ data/T4Q-2-light-hadrons/README.md
@@ -0,0 +1,1 @@
+Aplitudes for $T_{4Q}\to p \bar{p}$
